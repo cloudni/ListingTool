f8fbdb94a8f2b10448506ffee78860f9e3c596df
cghbs22 <cghbs22@126.com>
2015-04-13 13:51:23 +0800

    lt_transaction相关表从三月份迁移至四月份

--- DB_Script/2015_4/table_initial.sql
+++ DB_Script/2015_4/table_initial.sql
@@ -1,3 +1,88 @@
+/*start 2015/04/03*/
+ALTER TABLE `lt_company` ADD COLUMN `balance` decimal(10,2) NULL;
+
+DROP TABLE IF EXISTS `lt_transaction`;
+CREATE TABLE `lt_transaction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` int(11) NOT NULL,
+  `type` varchar(20) DEFAULT NULL COMMENT '支付平台类型，如paypal',
+  `transaction_type` varchar(20) DEFAULT NULL COMMENT '交易类型：存款、取款',
+  `status` varchar(20) DEFAULT NULL COMMENT '交易状态(业务状态，区别于支付状态）',
+  `contents` varchar(1000) DEFAULT NULL,
+  `payment_transaction_id` varchar(100) DEFAULT NULL COMMENT '支付平台的交易号',
+  `total` decimal(10,2) DEFAULT NULL COMMENT '发起交易的额度',
+  `fee` decimal(10,2) DEFAULT NULL COMMENT '交易平台扣款',
+  `net` decimal(10,2) DEFAULT NULL COMMENT '充值成功金额=total-fee',
+  `enabled` tinyint(1) DEFAULT '1',
+  `create_time_utc` int(11) DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time_utc` int(11) DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `lt_transaction_access_token`;
+CREATE TABLE `lt_transaction_access_token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `access_token` varchar(255) NOT NULL,
+  `lift_cycle` bigint(20) DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `create_time_utc` int(11) NOT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `lt_transaction_credit_card`;
+CREATE TABLE `lt_transaction_credit_card` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `transaction_id` int(11) DEFAULT NULL COMMENT '关联表li_transaction_detail.id',
+  `first_name` varchar(50) DEFAULT NULL COMMENT '持卡人名字',
+  `last_name` varchar(50) DEFAULT NULL COMMENT '持卡人姓',
+  `type` varchar(20) DEFAULT NULL COMMENT '卡类型：如visa',
+  `number` varchar(30) DEFAULT NULL COMMENT '信用卡号',
+  `expire_month` char(2) DEFAULT NULL COMMENT '到期月份',
+  `expire_year` char(4) DEFAULT NULL COMMENT '到期年份',
+  `cvv2` char(3) DEFAULT NULL COMMENT '卡验证码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `lt_transaction_payment_account`;
+CREATE TABLE `lt_transaction_payment_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `transaction_id` int(11) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL COMMENT '账号对应的邮箱',
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `payer_id` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `lt_transaction_paypal`;
+CREATE TABLE `lt_transaction_paypal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `transaction_id` int(11) DEFAULT NULL COMMENT '关联表lt_transaction_detail.id',
+  `interface_name` varchar(50) DEFAULT NULL COMMENT '接口名称',
+  `action_state` varchar(50) DEFAULT NULL COMMENT '动作对应状态：如created、approved',
+  `payment_id` varchar(100) DEFAULT NULL,
+  `payment_transaction_id` varchar(100) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `payment_state` varchar(50) DEFAULT NULL,
+  `platform_account_name` varchar(50) DEFAULT NULL,
+  `platform_account_state` varchar(20) DEFAULT NULL,
+  `total` decimal(10,2) DEFAULT NULL COMMENT '发起交易的额度',
+  `fee` decimal(10,2) DEFAULT NULL COMMENT '交易平台扣款',
+  `net` decimal(10,2) DEFAULT NULL COMMENT '充值成功金额=total-fee',
+  `request_content` text COMMENT '请求详情',
+  `response_content` text COMMENT '响应详情',
+  `response_error_content` text,
+  `created_time` varchar(50) DEFAULT NULL,
+  `approved_time` varchar(50) DEFAULT NULL,
+  `completed_time` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_lt_transaction_id` (`transaction_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*end 2015/04/03*/
+
 /*start 2015-04-07*/
 drop table if exists `lt_ad_campaign`;
 CREATE TABLE `lt_ad_campaign` (
